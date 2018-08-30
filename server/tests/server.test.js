be76a1dc93ed6a548b17852e60390c68de6f2a46
Vivek Kekare <vivek.k@intelegain.com>
2018-08-30 17:20:28 +0530

    Add GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -42,6 +42,7 @@ describe('POST /todos', () => {
 				if (err) {
 					return done(err);
 				}
+				
 				Todo.find().then((todos) => {
 					expect(todos.length).toBe(0);
 					expect(todos[0].text).toBe(text);
