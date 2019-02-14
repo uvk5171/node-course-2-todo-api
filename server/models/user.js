38ac738cf48301e8fa9a9de802e9a4ab31cf633a
Vivek Kekare <vivek.k@intelegain.com>
2019-02-14 15:57:15 +0530

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -5,83 +5,104 @@ const _ = require('lodash');
 const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1,
-    unique: true,
-    validate: {
-      validator: validator.isEmail,
-      message: '{VALUE} is not a valid email'
-    }
-  },
-  password: {
-    type: String,
-    require: true,
-    minlength: 6
-  },
-  tokens: [{
-    access: {
-      type: String,
-      required: true
-    },
-    token: {
-      type: String,
-      required: true
-    }
-  }]
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 1,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message: '{VALUE} is not a valid email'
+		}
+	},
+	password: {
+		type: String,
+		require: true,
+		minlength: 6
+	},
+	tokens: [{
+		access: {
+			type: String,
+			required: true
+		},
+		token: {
+			type: String,
+			required: true
+		}
+	}]
 });
 
 UserSchema.methods.toJSON = function () {
-  var user = this;
-  var userObject = user.toObject();
+	var user = this;
+	var userObject = user.toObject();
 
-  return _.pick(userObject, ['_id', 'email']);
+	return _.pick(userObject, ['_id', 'email']);
 };
 
 UserSchema.methods.generateAuthToken = function () {
-  var user = this;
-  var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+	var user = this;
+	var access = 'auth';
+	var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
-  user.tokens.push({access, token});
+	user.tokens.push({access, token});
 
-  return user.save().then(() => {
-    return token;
-  });
+	return user.save().then(() => {
+		return token;
+	});
 };
 
 UserSchema.statics.findByToken = function (token) {
-  var User = this;
-  var decoded;
+	var User = this;
+	var decoded;
 
-  try {
-    decoded = jwt.verify(token, 'abc123');
-  } catch (e) {
-    return Promise.reject();
-  }
+	try {
+		decoded = jwt.verify(token, 'abc123');
+	} catch (e) {
+		return Promise.reject();
+	}
 
-  return User.findOne({
-    '_id': decoded._id,
-    'tokens.token': token,
-    'tokens.access': 'auth'
-  });
+	return User.findOne({
+		'_id': decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth'
+	});
 };
 
-UserSchema.pre('save', function (next) {
-  var user = this;
+UserSchema.statics.findByCredentials = function (email, password) {
+	var User = this;
 
-  if (user.isModified('password')) {
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(user.password, salt, (err, hash) => {
-        user.password = hash;
-        next();
-      });
-    });
-  } else {
-    next();
-  }
+	return User.findOne({email}).then((user) => {
+		if (!user) {
+			return Promise.reject();
+		}
+
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (err, res) => {
+				if (res) {
+					resolve(user);
+				} else {
+					reject();
+				}
+			});
+		}) 
+	});
+};
+
+
+UserSchema.pre('save', function (next) {
+	var user = this;
+
+	if (user.isModified('password')) {
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash;
+				next();
+			});
+		});
+	} else {
+		next();
+	}
 });
 
 var User = mongoose.model('User', UserSchema);
--- server/server.js
+++ server/server.js
@@ -17,21 +17,21 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+	text: req.body.text
   });
 
   todo.save().then((doc) => {
-    res.send(doc);
+	res.send(doc);
   }, (e) => {
-    res.status(400).send(e);
+	res.status(400).send(e);
   });
 });
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos});
+	res.send({todos});
   }, (e) => {
-    res.status(400).send(e);
+	res.status(400).send(e);
   });
 });
 
@@ -39,17 +39,17 @@ app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
+	return res.status(404).send();
   }
 
   Todo.findById(id).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();
-    }
+	if (!todo) {
+	  return res.status(404).send();
+	}
 
-    res.send({todo});
+	res.send({todo});
   }).catch((e) => {
-    res.status(400).send();
+	res.status(400).send();
   });
 });
 
@@ -57,17 +57,17 @@ app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
+	return res.status(404).send();
   }
 
   Todo.findByIdAndRemove(id).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();
-    }
+	if (!todo) {
+	  return res.status(404).send();
+	}
 
-    res.send({todo});
+	res.send({todo});
   }).catch((e) => {
-    res.status(400).send();
+	res.status(400).send();
   });
 });
 
@@ -76,24 +76,24 @@ app.patch('/todos/:id', (req, res) => {
   var body = _.pick(req.body, ['text', 'completed']);
 
   if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
+	return res.status(404).send();
   }
 
   if (_.isBoolean(body.completed) && body.completed) {
-    body.completedAt = new Date().getTime();
+	body.completedAt = new Date().getTime();
   } else {
-    body.completed = false;
-    body.completedAt = null;
+	body.completed = false;
+	body.completedAt = null;
   }
 
   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();
-    }
+	if (!todo) {
+	  return res.status(404).send();
+	}
 
-    res.send({todo});
+	res.send({todo});
   }).catch((e) => {
-    res.status(400).send();
+	res.status(400).send();
   })
 });
 
@@ -103,16 +103,30 @@ app.post('/users', (req, res) => {
   var user = new User(body);
 
   user.save().then(() => {
-    return user.generateAuthToken();
+	return user.generateAuthToken();
   }).then((token) => {
-    res.header('x-auth', token).send(user);
+	res.header('x-auth', token).send(user);
   }).catch((e) => {
-    res.status(400).send(e);
+	res.status(400).send(e);
   })
 });
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+// POST /users/login
+
+app.post('/users/login', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+
+	User.findByCredentials(body.email, body.password).then((user) => {
+		return user.generateAuthToken().then((token) => {
+			res.header('x-auth', token).send(user);
+		});
+	}).catch((e) => {
+		res.status(400).send();
+	});
 });
 
 app.listen(port, () => {
